b5f8bdfe314d8d0f0c1b49410e4de26b4d84989b
hyejinjeong9999 <hyejinj810@gmail.com>
2021-12-11 20:16:40 +0900

    firstcommit

---
+++ src/main/java/com/banttang/banttangadminapi/BanttangAdminApiApplication.java
@@ -0,0 +1,13 @@
+package com.banttang.banttangadminapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanttangAdminApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanttangAdminApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/banttang/banttangadminapi/BanttangAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banttang.banttangadminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanttangAdminApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
