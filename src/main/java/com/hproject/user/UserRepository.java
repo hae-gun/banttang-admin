20c4321a918c9a2d6f1eff40af6220cb1b4aa744
Hae-gun <newchk610@gmail.com>
2021-12-21 22:25:55 +0900

    security setting

--- src/main/java/com/hproject/user/UserRepository.java
+++ src/main/java/com/hproject/user/UserRepository.java
@@ -1,2 +1,9 @@
-package com.hproject.user;public interface UserRepository {
+package com.hproject.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserInfo, Long> {
+    Optional<UserInfo> findByEmail(String email);
 }
--- src/main/java/com/hproject/user/dto/UserInfoDto.java
+++ src/main/java/com/hproject/user/dto/UserInfoDto.java
@@ -1,2 +1,13 @@
-package com.hproject.user.dto;public class UserInfoDto {
+package com.hproject.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserInfoDto {
+    private String email;
+    private String password;
+
+    private String auth;
 }
