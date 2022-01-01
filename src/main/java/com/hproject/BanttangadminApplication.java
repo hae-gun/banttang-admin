026f59bb2bee9dbbd3c30862fd1f2effa24b7a51
hae-gun <newchk610@gmail.com>
2022-01-01 23:18:49 +0900

    test

--- src/main/java/com/hproject/BanttangadminApplication.java
+++ src/main/java/com/hproject/BanttangadminApplication.java
@@ -1,6 +1,7 @@
 package com.hproject;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
--- src/main/java/com/hproject/user/UserController.java
+++ src/main/java/com/hproject/user/UserController.java
@@ -3,6 +3,7 @@ package com.hproject.user;
 import com.hproject.user.dto.UserInfoDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("/user")
+    @GetMapping("/user")
     public String signup(UserInfoDto infoDto) { // 회원 추가
         userService.save(infoDto);
         return "redirect:/login";
