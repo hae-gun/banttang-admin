1bfbd7035ffecee5ff70dd0f2bc5a430d0b97b1f
hae-gun <newchk610@gmail.com>
2022-01-02 23:07:35 +0900

    reset project

--- src/main/java/com/hproject/security/WebSecurityConfig.java
+++ src/main/java/com/hproject/security/WebSecurityConfig.java
@@ -3,42 +3,37 @@ package com.hproject.security;
 import com.hproject.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final UserService userService;
+
 
     @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception { // 5
-                http
-//                .csrf().disable()
-                .authorizeRequests() // 6
-                .antMatchers("/login", "/signup", "/user", "/home","/static/login").permitAll() // 누구나 접근 허용
-                .antMatchers("/").hasRole("USER") // USER, ADMIN만 접근 가능
-                .antMatchers("/admin").hasRole("ADMIN") // ADMIN만 접근 가능
-                .anyRequest().authenticated()
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .formLogin().disable();// 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .antMatchers("/api/v1/test/permit-all").permitAll()
+                .antMatchers("/api/v1/test/auth").authenticated()
+                .antMatchers("/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin().disable();
     }
 
-//    @Override
-//    public void configure(AuthenticationManagerBuilder auth) throws Exception { // 9
-//        auth.userDetailsService(userService)
-//                 해당 서비스(userService)에서는 UserDetailsService를 implements해서
-//                 loadUserByUsername() 구현해야함 (서비스 참고)
-//                .passwordEncoder(new BCryptPasswordEncoder());
-//    }
 }
---
+++ src/main/java/com/hproject/test/TestService.java
@@ -0,0 +1,11 @@
+package com.hproject.test;
+
+public class TestService {
+    public Object getTest() {
+        return "test1";
+    }
+
+    public Object getTest2() {
+        return "test2";
+    }
+}
