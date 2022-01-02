e14e4472b0b2523209f2a26a1d9c20f0809c29a8
hae-gun <newchk610@gmail.com>
2022-01-02 22:47:50 +0900

    login page disable" "

--- src/main/java/com/hproject/security/WebSecurityConfig.java
+++ src/main/java/com/hproject/security/WebSecurityConfig.java
@@ -24,20 +24,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception { // 5
                 http
-                .csrf().disable()
+//                .csrf().disable()
                 .authorizeRequests() // 6
-                .antMatchers("/login", "/signup", "/user", "/home").permitAll() // 누구나 접근 허용
+                .antMatchers("/login", "/signup", "/user", "/home","/static/login").permitAll() // 누구나 접근 허용
                 .antMatchers("/").hasRole("USER") // USER, ADMIN만 접근 가능
                 .antMatchers("/admin").hasRole("ADMIN") // ADMIN만 접근 가능
-                .anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
+                .anyRequest().authenticated()
                 .and()
-                .formLogin() // 7
-                .loginPage("/static/login") // 로그인 페이지 링크
-                .defaultSuccessUrl("/") // 로그인 성공 후 리다이렉트 주소
-                .and()
-                .logout() // 8
-                .logoutSuccessUrl("/login") // 로그아웃 성공시 리다이렉트 주소
-                .invalidateHttpSession(true); // 세션 날리기
+                .formLogin().disable();// 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
     }
 
 //    @Override
